897c3ec9ed7cbe159403cf5f1dbc5359b52b5833
Jhanpiere Montes <jhanpieremontes@gmail.com>
2022-09-10 13:47:58 -0500

    Se avanzó el proyecto queda pendiente agregar el boton detener y la alarma

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 const currentTimeDisplay = document.getElementById("current-time-display")
-const alarmTimeDisplau = document.getElementById("alarm-time-display")
+const alarmTimeDisplay = document.getElementById("alarm-time-display")
 const alarmMessageDisplay = document.getElementById("alarm-message-display")
 document.getElementById("set-alarm-button").addEventListener("click", setAlarm)
 let currenTime = null
 let alarmTime = null
-const audio = new Audio('bella.mp3');
+const audio = new Audio('ring-super-mario-bros.mp3');
 
 function renderCurrentTime() {
-    const today = new Date();
-    const now = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    console.log(now)
+    setInterval(() => {
+        currenTime = new Date().toLocaleTimeString();
+        currentTimeDisplay.textContent = currenTime;
+        alarmChecker()
+    }, 1000)
 }
 renderCurrentTime()
 
 function setAlarm() {
+    event.preventDefault()
+    let wakeyHours = document.getElementById("hours-input").value
+    let wakeyMins = document.getElementById("minutes-input").value
 
+    if(wakeyMins / 10 < 1) {
+        wakeyMins = "0" + wakeyMins
+    }
+    if(wakeyHours / 10 < 1) {
+        wakeyHours = "0" + wakeyHours
+    }
+
+    alarmTime = `${wakeyHours}:${wakeyMins}:00`
+    alarmTimeDisplay.textContent = alarmTime
 }
 
 function alarmChecker() {
-
+    if (alarmTime === currentTime) {
+        ringTheAlarm()
+    }
 }
-alarmChecker()
 
 function ringTheAlarm() {
+    audio.play()
+    alarmMessageDisplay.textContent = 'Get Up!!!'
+}
 
+function stopAlarm() {
+    audio.stop()
+    alarmMessageDisplay.textContent = ""
 }
